97ed3d876b310e4b2993ab503464434c25199a0c
彭冲 <1058472896@qqcom>
2019-05-06 07:30:21 +0800

    model示例

---
+++ src/main/java/nefu/snow/model/User.java
@@ -0,0 +1,13 @@
+package nefu.snow.model;
+
+import lombok.Data;
+
+/**
+ * 示例
+ */
+@Data
+public class User {
+    private Integer user_id;
+    private String userName;
+    private String password;
+}
